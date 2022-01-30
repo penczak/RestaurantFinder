de6b89a4049cf95bb8dfbed9485769725ade934a
Daniel Penczak <penczakdaniel@gmail.com>
2022-01-30 13:17:55 -0600

    Pasted fetchData function into Detail.jsx

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import RestaurantFinder from '../APIs/RestaurantFinder';
 
 const Detail = () => {
-  return <div>
-      
-  </div>;
+
+    const { id } = useParams();
+    let navigate = useNavigate(); //navigate instead of history = useHistory in newer versions of react-router-dom
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [priceRange, setPriceRange] = useState("");
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+
+                const response = await RestaurantFinder.get(`/${id}`);
+                console.log(response.data.data);
+                setName(response.data.data.restaurants.name);
+                setLocation(response.data.data.restaurants.location);
+                setPriceRange(response.data.data.restaurants.price_range);
+            }
+            catch (err) { console.log(err) }
+        }
+
+        fetchData();
+    }, [id]);
+
+    return (
+    <div>
+        <h1 className='text-center'>{name} 🍔</h1>
+    </div>
+    );
 };
 
 export default Detail;
--- client/src/routes/RestaurantDetail.jsx
+++ client/src/routes/RestaurantDetail.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import HomeButton from "../components/HomeButton";
+import Detail from "../components/Detail";
 
 const RestaurantDetail = () => {
     return (
         <div>
             <HomeButton />
-            Detail View
+            <Detail />
         </div>
     )
-}
+};
 
 export default RestaurantDetail;
